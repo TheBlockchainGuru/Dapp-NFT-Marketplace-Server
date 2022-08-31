18343a4c0727db3a6057a2ebc61b9b4d13724051
cryptodev26 <cryptodev26@gmail.com>
2022-08-31 19:35:46 +0300

    search backend

--- routes/api/nfts.js
+++ routes/api/nfts.js
@@ -2,15 +2,6 @@ const express = require('express');
 const router = express.Router();
 const nftModule = require('../../modules/nft/nftController');
 
-// router.post('/detail/:id', userModule.)
-
-/**
- * @route POST api/user/update
- * @description Update user route
- * @access Public
- */
-router.post('/search', nftModule.Search);
-
 /**
  * @route GET api/nft/total-mint-count
  * @description Get total minted nft count
